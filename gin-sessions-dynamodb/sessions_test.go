a1d49ad1061ab3419106748e1d8f26936a8e5880
Henry <5065089+nguyengg@users.noreply.github.com>
2025-02-23 15:51:16 -0800

    unexport MockDynamoDBClient

--- gin-sessions-dynamodb/sessions_test.go
+++ gin-sessions-dynamodb/sessions_test.go
@@ -1,6 +1,7 @@
 package sessions
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,13 +17,38 @@ type TestSession struct {
 	SessionId string `dynamodbav:"sessionId,hashkey" tableName:"session"`
 }
 
+type MockManagerAPIClient struct {
+	mock.Mock
+	dynamodb.Client
+}
+
+func (m *MockManagerAPIClient) DeleteItem(ctx context.Context, params *dynamodb.DeleteItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.DeleteItemOutput, error) {
+	args := m.Called(ctx, params, optFns)
+	return args.Get(0).(*dynamodb.DeleteItemOutput), args.Error(1)
+}
+
+func (m *MockManagerAPIClient) GetItem(ctx context.Context, params *dynamodb.GetItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.GetItemOutput, error) {
+	args := m.Called(ctx, params, optFns)
+	return args.Get(0).(*dynamodb.GetItemOutput), args.Error(1)
+}
+
+func (m *MockManagerAPIClient) PutItem(ctx context.Context, params *dynamodb.PutItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.PutItemOutput, error) {
+	args := m.Called(ctx, params, optFns)
+	return args.Get(0).(*dynamodb.PutItemOutput), args.Error(1)
+}
+
+func (m *MockManagerAPIClient) UpdateItem(ctx context.Context, params *dynamodb.UpdateItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.UpdateItemOutput, error) {
+	args := m.Called(ctx, params, optFns)
+	return args.Get(0).(*dynamodb.UpdateItemOutput), args.Error(1)
+}
+
 func TestSessions_New(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	w := httptest.NewRecorder()
 	c, r := gin.CreateTestContext(w)
 
 	sid := "my-session-id"
-	client := &MockDynamoDBClient{}
+	client := &MockManagerAPIClient{}
 
 	r.GET("/", Sessions[TestSession]("my-session", func(s *Session) {
 		s.Client = client
@@ -44,7 +70,7 @@ func TestSessions_Get(t *testing.T) {
 	c, r := gin.CreateTestContext(w)
 
 	sid := "my-session-id"
-	client := &MockDynamoDBClient{}
+	client := &MockManagerAPIClient{}
 	client.
 		On("GetItem", mock.Anything, mock.MatchedBy(func(input *dynamodb.GetItemInput) bool {
 			assert.Equal(t, sid, input.Key["sessionId"].(*types.AttributeValueMemberS).Value)
