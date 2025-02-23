65e7c2c2ad37417cd64dad8187881ee04ab446e9
Henry <5065089+nguyengg@users.noreply.github.com>
2025-02-23 15:25:04 -0800

    remove unused functions

--- lambda/gin-function-url/error.go
+++ lambda/gin-function-url/error.go
@@ -1,57 +1,10 @@
 package ginadapter
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
-
-// AbortJSON aborts the current request with http.StatusInternalServerError and returns a generic JSON error message.
-//
-// The error message will have this format;
-//
-//	{
-//		"status": 500,
-//		"message": "Internal Server Error"
-//	}
-func AbortJSON(c *gin.Context) {
-	c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-		"status":  http.StatusInternalServerError,
-		"message": http.StatusText(http.StatusInternalServerError),
-	})
-}
-
-// AbortWithStatusJSON aborts the current request with the given status and the message comes from the default status
-// text for the message.
-//
-// The error message will have this format;
-//
-//	{
-//		"status": status,
-//		"message": http.StatusText(status)
-//	}
-func AbortWithStatusJSON(c *gin.Context, status int) {
-	c.AbortWithStatusJSON(status, gin.H{
-		"status":  status,
-		"message": http.StatusText(status),
-	})
-}
-
-// AbortWithStatusJSONf aborts the current request with the given status and formatted message.
-//
-// The error message will have this format;
-//
-//	{
-//		"status": status,
-//		"message": fmt.Sprintf(format, a...)
-//	}
-func AbortWithStatusJSONf(c *gin.Context, status int, format string, a ...any) {
-	c.AbortWithStatusJSON(status, gin.H{
-		"status":  status,
-		"message": fmt.Sprintf(format, a...),
-	})
-}
 
 // JSONErrorHandler is a middleware that makes sure errors are always returned to caller as JSON content.
 //
@@ -89,7 +42,6 @@ func JSONErrorHandler() gin.HandlerFunc {
 
 		switch err.Type {
 		case gin.ErrorTypeBind, gin.ErrorTypePublic:
-			err.MarshalJSON()
 			c.JSON(status, gin.H{
 				"status":  status,
 				"message": err.Error(),
