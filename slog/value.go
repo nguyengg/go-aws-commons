cc108bea7bd9e94a86180b1f098fe8b2a3bb34ef
Henry <5065089+nguyengg@users.noreply.github.com>
2026-02-21 21:29:54 -0800

    feat(commons): add convenient slog.Value wrappers for JSON

---
+++ slog/value.go
@@ -0,0 +1,59 @@
+package slog
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"log/slog"
+)
+
+// JSONValue returns a slog.Value that will cause slog.JSONHandler to print the data as-is if it is valid JSON.
+//
+// If the logger isn't using slog.JSONHandler, or if data is not valid JSON, then the default logic for printing bytes
+// is used, which should be printing its base64 standard encoding.
+//
+// Useful if you want to log a response from a GET call for example that you expect to be JSON, but you don't want to
+// parse it into valid JSON first.
+func JSONValue(data []byte) slog.Value {
+	if json.Valid(data) {
+		return slog.AnyValue(jsonValue{data})
+	}
+
+	return slog.AnyValue(data)
+}
+
+// JSONStringValue is a variant of JSONValue that receives a string instead.
+//
+// If the raw bytes of the string argument is not valid JSON, or if the logger isn't using slog.JSONHandler, the default
+// logic for printing string will be used.
+func JSONStringValue(s string) slog.Value {
+	data := []byte(s)
+	if json.Valid(data) {
+		return slog.AnyValue(jsonStringValue{data})
+	}
+
+	return slog.StringValue(s)
+}
+
+type jsonValue struct {
+	data []byte
+}
+
+func (j jsonValue) String() string {
+	return base64.StdEncoding.EncodeToString(j.data)
+}
+
+func (j jsonValue) MarshalJSON() ([]byte, error) {
+	return j.data, nil
+}
+
+type jsonStringValue struct {
+	data []byte
+}
+
+func (j jsonStringValue) String() string {
+	return string(j.data)
+}
+
+func (j jsonStringValue) MarshalJSON() ([]byte, error) {
+	return j.data, nil
+}
---
+++ slog/value_test.go
@@ -0,0 +1,132 @@
+package slog
+
+import (
+	"bytes"
+	"log/slog"
+	"testing"
+	"testing/synctest"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJSONStringValue(t *testing.T) {
+	tests := []struct {
+		name string
+		args string
+		want string
+	}{
+		{
+			name: "valid JSON",
+			args: `{"6": 7}`,
+			want: `{"time":"1999-12-31T16:00:00-08:00","level":"INFO","msg":"test","args":{"6":7}}`,
+		},
+		{
+			name: "invalid JSON",
+			args: `hello, world!`,
+			want: `{"time":"1999-12-31T16:00:00-08:00","level":"INFO","msg":"test","args":"hello, world!"}`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			synctest.Test(t, func(t *testing.T) {
+				var buf bytes.Buffer
+				logger := slog.New(slog.NewJSONHandler(&buf, nil))
+				logger.Info("test", slog.Any("args", JSONStringValue(tt.args)))
+
+				got := buf.String()
+				assert.JSONEqf(t, tt.want, got, "JSONStringValue(%v)", tt.args)
+			})
+		})
+	}
+}
+
+func TestJSONStringValue_NoJSONHandler(t *testing.T) {
+	tests := []struct {
+		name string
+		args string
+		want string
+	}{
+		{
+			name: "valid JSON",
+			args: `{"6": 7}`,
+			want: "time=1999-12-31T16:00:00.000-08:00 level=INFO msg=test args=\"{\\\"6\\\": 7}\"\n",
+		},
+		{
+			name: "invalid JSON",
+			args: `hello, world!`,
+			want: "time=1999-12-31T16:00:00.000-08:00 level=INFO msg=test args=\"hello, world!\"\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			synctest.Test(t, func(t *testing.T) {
+				var buf bytes.Buffer
+				logger := slog.New(slog.NewTextHandler(&buf, nil))
+				logger.Info("test", slog.Any("args", JSONStringValue(tt.args)))
+
+				assert.Equalf(t, tt.want, buf.String(), "JSONStringValue(%v)", tt.args)
+			})
+		})
+	}
+}
+
+func TestJSONValue(t *testing.T) {
+	tests := []struct {
+		name string
+		args string
+		want string
+	}{
+		{
+			name: "valid JSON",
+			args: `{"6": 7}`,
+			want: `{"time":"1999-12-31T16:00:00-08:00","level":"INFO","msg":"test","args":{"6":7}}`,
+		},
+		{
+			name: "invalid JSON",
+			args: `hello, world!`,
+			want: `{"time":"1999-12-31T16:00:00-08:00","level":"INFO","msg":"test","args":"aGVsbG8sIHdvcmxkIQ=="}`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			synctest.Test(t, func(t *testing.T) {
+				var buf bytes.Buffer
+				logger := slog.New(slog.NewJSONHandler(&buf, nil))
+				logger.Info("test", slog.Any("args", JSONValue([]byte(tt.args))))
+
+				got := buf.String()
+				assert.JSONEqf(t, tt.want, got, "JSONValue(%v)", tt.args)
+			})
+		})
+	}
+}
+
+func TestJSONValue_NoJSONHandler(t *testing.T) {
+	tests := []struct {
+		name string
+		args string
+		want string
+	}{
+		{
+			name: "valid JSON",
+			args: `{"6": 7}`,
+			want: "time=1999-12-31T16:00:00.000-08:00 level=INFO msg=test args=\"eyI2IjogN30=\"\n",
+		},
+		{
+			name: "invalid JSON",
+			args: `hello, world!`,
+			want: "time=1999-12-31T16:00:00.000-08:00 level=INFO msg=test args=\"hello, world!\"\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			synctest.Test(t, func(t *testing.T) {
+				var buf bytes.Buffer
+				logger := slog.New(slog.NewTextHandler(&buf, nil))
+				logger.Info("test", slog.Any("args", JSONValue([]byte(tt.args))))
+
+				assert.Equalf(t, tt.want, buf.String(), "JSONValue(%v)", tt.args)
+			})
+		})
+	}
+}
