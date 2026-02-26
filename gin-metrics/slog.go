408c88b1a2a313e8e28ac26e7d915f97cc73e825
Henry <5065089+nguyengg@users.noreply.github.com>
2026-02-25 21:58:28 -0800

    feat(gin-metrics): implement GetLogger

--- gin-metrics/slog.go
+++ gin-metrics/slog.go
@@ -8,15 +8,32 @@ import (
 
 const slogLoggerKey = "nguyengg/gin-metrics/slogLoggerKey"
 
-// Slog returns the slog.Logger instance associated with context.
+// Deprecated: use GetLogger for a more meaningful name.
+func Slog(c *gin.Context) *slog.Logger {
+	return GetLogger(c)
+}
+
+// GetLogger returns the slog.Logger instance associated with context.
 //
-// If none is available, return slog.Default.
+// If none is available, return slog.Default. The logger can be modified further using withFns; any changes here will be
+// attached back to gin.Context, and the modified logger is also returned.
 //
 // Equivalent to zerolog.Ctx except only receive gin.Context.
-func Slog(c *gin.Context) *slog.Logger {
-	if logger, ok := c.Get(slogLoggerKey); ok {
-		return logger.(*slog.Logger)
+func GetLogger(c *gin.Context, withFns ...func(*slog.Logger) *slog.Logger) *slog.Logger {
+	var logger *slog.Logger
+	if v, ok := c.Get(slogLoggerKey); ok {
+		logger = v.(*slog.Logger)
+	} else {
+		logger = slog.Default()
 	}
 
-	return slog.Default()
+	if len(withFns) != 0 {
+		for _, fn := range withFns {
+			logger = fn(logger)
+		}
+
+		c.Set(slogLoggerKey, logger)
+	}
+
+	return logger
 }
