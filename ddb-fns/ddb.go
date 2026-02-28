6cf0c6c0538c19b30c8d27ea37816c47b7bb2a28
Henry <5065089+nguyengg@users.noreply.github.com>
2026-02-28 01:21:21 -0800

    chore(ddb-fns): explicitly retract/deprecate this module

---
+++ ddb-fns/ddb.go
@@ -0,0 +1,6 @@
+// Package ddbfns is largely replaced by [github.com/nguyengg/go-aws-commons/ddb].
+//
+// Deprecated: replaced by [github.com/nguyengg/go-aws-commons/ddb].
+//
+// [github.com/nguyengg/go-aws-commons/ddb]: https://pkg.go.dev/github.com/nguyengg/go-aws-commons/ddb
+package ddbfns
---
+++ ddb-fns/go.mod
@@ -0,0 +1,5 @@
+module github.com/nguyengg/go-aws-commons/ddb-fns
+
+go 1.25.0
+
+retract [v0.0.0-0, v0.0.1]
